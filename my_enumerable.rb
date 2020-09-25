19bd25c695439d2a1b0af58e9fbf3ca725d534ed
jhdezj <jorgehdezj18@gmail.com>
2020-09-25 08:47:25 -0500

    refactor my_each_with_index method

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -12,11 +12,12 @@ module Enumerable
   def my_each_with_index
     return to_enum(_method_) unless block_given?
 
-    for index in self
-      yield(self, index)
+    i = 0
+    until i == length
+      yield(self[i], i)
+      i += 1
     end
   end
-  self
 end
 
-[1, 2, 3, 4].my_each { |x| print x  }
+[1, 2, 3, 4].my_each_with_index { |x, y| print y  }
